8b1569ffa6abd7f082a6ad09c70ea8f6f5f0d650
*.* <free@hong.kong>
2023-02-18 02:36:31 +0800

    extended failure case

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 description = "my project description";
 
 inputs .nixpkgs .url = "github:NixOS/nixpkgs";
-inputs .dream2nix .url = "github:nix-community/dream2nix";
+inputs .dream2nix .url = "github:nix-community/dream2nix?rev=77dbc655cda5cb00c85b99aed62aefb0edfa0f48";
 
 outputs =
 	{ self, nixpkgs, dream2nix }:
